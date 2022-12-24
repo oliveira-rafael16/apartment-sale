f833f5ab93ea3ff88c7eba666f06a98f4d3a311b
Rafael Baccarat <rafabaccarat@gmail.com>
2022-12-24 13:33:47 -0300

    First commit

---
+++ src/pages/apartment/index.tsx
@@ -0,0 +1,11 @@
+import styles from './apartment.module.scss';
+
+export default function Apartment() {
+    return (
+        <main>
+            <nav className="sale">
+
+            </nav>
+        </main>
+    )
+}
